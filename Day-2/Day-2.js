5667e4b98b4ee0791e750db3c5739b3751b3a748
mohdaneesh018 <mdaneesh537@gmail.com>
2026-03-03 16:48:31 +0530

    Practice

---
+++ Day-2/Day-2.js
@@ -0,0 +1,65 @@
+Qs. 1. Print pattern star
+* * * *
+* * *
+* * 
+*
+
+function isPattern(n) {
+    for (let i = n; i >= 1; i--) {
+        let res = "";
+
+        for (let j = 1; j <= i; j++) {
+            res += "* "
+        }
+        console.log(res)
+    } 
+}
+isPattern(4);
+
+
+
+
+
+Qs. 2. Print Pattern Number
+1
+2 2
+3 3 3 
+4 4 4 4 
+
+function isPattern(n) {
+    for (let i = 1; i <= n; i++) {
+        let res = ""
+        for (let j = 1; j <= i; j++) {
+            res += i + " "
+        }
+        console.log(res)
+    }
+}
+isPattern(4);
+
+
+
+
+
+Qs. 3. Print Pattern Number
+1
+1 2
+1 2 3 
+1 2 3 4 
+1 2 3 4 5 
+
+function isPattern(n) {
+    for (let i = 1; i <= n; i++) {
+        let res = ""
+        for (let j = 1; j <= i; j++) {
+            res += j + " "
+        }
+        console.log(res)
+    }
+}
+isPattern(5);
+
+
+
+
+
